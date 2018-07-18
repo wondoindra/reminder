cdcfe5952484a7b02cbc7c33c9f63906cca44185
Wondo Indra <wondoindra28@gmail.com>
2018-07-18 16:08:40 +0700

    Added animation when deleting task

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
     $("#input-button").click(function () {
-        var text = $("#input-text").val();
-        $(".reminder-list-container").append("<div class='reminder-list'>" +
-            "<button id='reminder-delete' class='reminder-list-button'>X</button> " +
-            "<li>" + text + "</li>" + "</div>");
-        $("#input-text").val('');
+        if ($("#input-text").val().length === 0) {
+            alert("Write something ");
+        } else {
+            var text = $("#input-text").val();
+            $(".reminder-list-container").append("<div class='reminder-list'>" +
+                "<button id='reminder-delete' class='reminder-list-button'>X</button> " +
+                "<li>" + text + "</li>" + "</div>");
+            $("#input-text").val('');
+        }
     });
 });
 
 $(document).on("click", "#reminder-delete", function () {
-    $(this).parent().hide();
+    $(this).parent().fadeOut(400);
 })
