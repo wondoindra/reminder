7af035d9c479d22e9601816c1ad53410ce1ae529
Wondo Indra <wondoindra28@gmail.com>
2018-07-19 15:31:29 +0700

    added date time

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     $("#input-button").click(function () {
-        if ($("#input-text").val().length === 0) {
+        if ($("#input-text").val().length && $("#datetimepicker").val().length === 0) {
             alert("Write something ");
         } else {
             var text = $("#input-text").val();
+            var datetime = $("#datetimepicker").val();
             $(".reminder-list-container").append("<div id='list-div' class='alert alert-light clearfix align-middle m-3 border-danger'>" +
-                text + "<button id='reminder-ongoing' class='btn btn-danger float-right' disabled>Ongoing</button>" +
+                datetime + "<hr>" + text + "<button id='reminder-ongoing' class='btn btn-danger float-right' disabled>Ongoing</button>" +
                 "<button id='reminder-completed' class='btn mr-2 float-right'>Completed</button>" + "</div>");
             $("#input-text").val('');
         }
