ea0d76b8e0e115cf3a04ac772899ee169caf9575
Wondo Indra <wondoindra28@gmail.com>
2018-07-19 14:59:56 +0700

    Added reminder with status border and status button

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ $(document).ready(function () {
             alert("Write something ");
         } else {
             var text = $("#input-text").val();
-            $(".reminder-list-container").append("<div class='card m-3'>" +
-                "<div class='card-header'>" + "</div>" + "<div class='card-body'>" +
-                "<p class='card-text'>" + text + "</p>" + "<button id='reminder-delete' class='btn btn-danger float-right'>Completed</button>" + "</div>" + "</div>");
+            $(".reminder-list-container").append("<div id='list-div' class='alert alert-light clearfix align-middle m-3 border-danger'>" +
+                text + "<button id='reminder-ongoing' class='btn btn-danger float-right' disabled>Ongoing</button>" +
+                "<button id='reminder-completed' class='btn mr-2 float-right'>Completed</button>" + "</div>");
             $("#input-text").val('');
         }
     });
 });
 
-$(document).on("click", "#reminder-delete", function () {
-    $(".card").fadeOut(400);
-})
+$(document).on("click", "#reminder-ongoing", function () {
+    $(this).addClass("btn-danger").prop("disabled", true);
+    $("#reminder-completed").removeClass("btn-success").prop("disabled", null);
+    $("#list-div").removeClass("border-success").addClass("border-danger");
+});
+
+$(document).on("click", "#reminder-completed", function () {
+    $(this).addClass("btn-success").prop("disabled", true)
+    $("#reminder-ongoing").removeClass("btn-danger").prop("disabled", null);
+    $("#list-div").removeClass("border-danger").addClass("border-success");
+});
